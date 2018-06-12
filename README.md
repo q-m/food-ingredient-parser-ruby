b67252e22d2519cf6682163fac22c7701fd23d95
wvengen <willem@thequestionmark.org>
2018-06-12 14:53:51 +0200

    Improve test tool

--- README.md
+++ README.md
@@ -37,6 +37,44 @@ Results in
 ]
 ```
 
+## Test tool
+
+```
+$ ./test.rb -h
+Usage: ./test.rb [options] --file|-f <filename>
+       ./test.rb [options] --string|-s <ingredients>
+
+    -f, --file FILE                  Parse all lines of the file as ingredient lists.
+    -s, --string INGREDIENTS         Parse specified ingredient list.
+    -q, --[no-]quiet                 Only show summary.
+    -v, --[no-]verbose               Show more data (parsed tree).
+    -h, --help                       Show this help
+
+$ ./test.rb -v -s "tomato"
+"tomato"
+RootNode+Ingredients0 offset=0, "tomato" (contains):
+  SyntaxNode offset=0, ""
+  ListNode+List13 offset=0, "tomato" (contains):
+    SyntaxNode+List12 offset=0, "tomato" (ingredient):
+      IngredientNode+SimpleIngredient1 offset=0, "tomato" (name):
+        SyntaxNode+SimpleIngredient0 offset=0, "tomato" (char):
+          SyntaxNode offset=0, "t"
+          SyntaxNode offset=1, "omato":
+            SyntaxNode offset=1, "o"
+            SyntaxNode offset=2, "m"
+            SyntaxNode offset=3, "a"
+            SyntaxNode offset=4, "t"
+            SyntaxNode offset=5, "o"
+      SyntaxNode offset=6, ""
+  SyntaxNode offset=6, ""
+  SyntaxNode offset=6, ""
+  SyntaxNode offset=6, ""
+[{:name=>"tomato"}]
+
+$ ./test.rb -q -f data/test-cases
+parsed 15, no result 0
+```
+
 ## Test data
 
 [data/ingredient-samples-nl](`data/ingredient-samples-nl`) contains about 150k
--- test.rb
+++ test.rb
@@ -2,83 +2,77 @@
 #
 # Parser for food ingredient lists.
 #
-require 'pry'
+require 'optparse'
 require_relative 'ingredients-parser'
 
-parser = IngredientsParser.new
+begin
+  require 'pry'
+rescue loaderror
+  # ok, we have a fallback
+end
 
-def parse_show(s, parsed, inspect: false)
-  puts "\e[1;32mEXAMPLE: \e[0;32m\"#{s}\"\e[0;22m"
+def pp(o)
+  if defined?(Pry)
+    Pry::ColorPrinter.pp(o)
+  else
+    puts(o.inspect)
+  end
+end
+
+def parse_single(s, parsed=nil, parser: nil, verbosity: 1)
+  puts "\e[0;32m\"#{s}\"\e[0;22m" if verbosity > 0
+  parser ||= IngredientsParser.new
+  parsed ||= parser.parse(s)
 
   if parsed
-    puts(parsed.inspect) if inspect
-    Pry::ColorPrinter.pp(parsed.to_a)
+    puts(parsed.inspect) if verbosity > 1
+    pp(parsed.to_a) if verbosity > 0
   else
-    puts "(no result)"
+    puts "(no result)" if verbosity > 0
   end
 end
 
-def test_samples(parser, inspect: false)
-  good = bad = 0
-  File.foreach('data/ingredient-samples-nl-excerpt').with_index do |line, linenum|
+def parse_file(path, parser: nil, verbosity: 1)
+  count_parsed = count_noresult = 0
+  File.foreach(path) do |line|
+    next if line =~ /^#/ # comment
+
     line = line.gsub('\\n', "\n").strip
-    parsed = parser.parse(line.strip)
-    good += 1 if parsed
-    bad += 1 unless parsed
+    parsed = parser.parse(line)
+    count_parsed += 1 if parsed
+    count_noresult += 1 unless parsed
 
-    parse_show(line, parsed, inspect: inspect) unless parsed
+    parse_single(line, parsed, parser: parser, verbosity: verbosity)
   end
-  puts "good \e[1;32m#{good}\e[0;22m, bad \e[1;31m#{bad}\e[0;22m"
+
+  puts "parsed \e[1;32m#{count_parsed}\e[0;22m, no result \e[1;31m#{count_noresult}\e[0;22m"
 end
 
-def test_tests(parser, inspect: false)
-  examples = {
-        "" => [],
-        "tomaat" => [{name: "tomaat"}],
-        "ingredienten: tomaat" => [{name: "tomaat"}],
-        "tomaat; komkommer" => [{name: "tomaat"}, {name: "komkommer"}],
-        "tomaat, komkommer" => [{name: "tomaat"}, {name: "komkommer"}],
-        "tomaat. komkommer." => [{name: "tomaat"}, {name: "komkommer"}],
-        "tomaat, komkommer: groen" => [{name: "tomaat"}, {name: "komkommer", contains: [{name: "groen"}]}],
-        "rode tomaat, groene komkommer" => [{name: "rode tomaat"}, {name: "groene komkommer"}],
-        "saus (tomaat, zout)" => [{name: "saus", contains: [{name: "tomaat"}, {name: "zout"}]}],
-        "saus [tomaat, zout]" => [{name: "saus", contains: [{name: "tomaat"}, {name: "zout"}]}],
-        "saus (tomaat, zout), water" => [{name: "saus", contains: [{name: "tomaat"}, {name: "zout"}]}, {name: "water"}],
-        "p (a, b), q(c)" => [{name: "p", contains: [{name: "a"}, {name: "b"}]}, {name: "q", contains: [{name: "c"}]}],
-        "p (a, b (r)), q(c)" => [{name: "p", contains: [{name: "a"}, {name: "b", contains: [{name: "r"}]}]}, {name: "q", contains: [{name: "c"}]}],
-        "p: a, b. q: c" => [{name: "p", contains: [{name: "a"}, {name: "b"}]}, {name: "q", contains: [{name: "c"}]}],
-        "p: a, b. q: c." => [{name: "p", contains: [{name: "a"}, {name: "b"}]}, {name: "q", contains: [{name: "c"}]}],
-        "o, p: a/b, q: c" => [{name: "o"}, {name: "p", contains: [{name: "a"}, {name: "b"}]}, {name: "q", contains: [{name: "c"}]}],
-        #"Met kip: vlees en dierlijke bijproducten (waarvan 4% kip), granen, mineralen. Met gevogelte: vlees en dierlijke bijproducten (waarvan 4% gevogelte), granen, mineralen." => nil,
-        #"Cacaomassa, suiker, 16% mangovulling [suiker, glucosestroop, 1% mangopuree, volle melkpoeder (LACTOSE, MELKEIWIT), cacaoboter, invertsuikerstroop, limoensap, alcohol, natuurlijk aroma, emulgator (lecithine (SOJA)), verdikkingsmiddel ( johannesbroodpitmeel, guarpitmeel, xanthaangom), kleurstof (caroteen)], 16% chilismaakvulling [suiker, palmvet, raapolie, magere cacaopoeder, cacaomassa, magere melkpoeder (LACTOSE, MELKEIWIT), Lactose (LACTOSE, MELKEIWIT), volle melkpoeder (LACTOSE, MELKEIWIT), wei-eiwit (LACTOSE, MELKEIWIT), dextrose, natuurlijk aroma, emulgator (lecithine (SOJA)), antioxidant (E306)], cacaoboter, emulgator [lecithine (SOJA)]. " => nil,
-        #"suiker, WEIPOEDER (MELK), emulgator: SOJALECITHINE, aroma, Cacaogehalte: 30% minimum, Kan sporen van pinda, noten bevatten" => nil,
-        #"{RIBSTER MINI} (44% mechanisch gescheiden varkensvlees, {tarwe}gluten, ketjap (water, aroma ({soja}), kleurstof: E150d), {soja}-eiwit), {KIPKORN MINI} (28% mechanisch gescheiden kippenvlees, water, {tarwe}gluten, antioxidant: ascorbinezuur, Kan sporen van {ei} bevatten.)" => nil,
-        #"30% pompelmoezen sap. water. zuurteregelaar: citroenzuur. natuurlijke aroma's." => nil,
-        #"TARWEBLOEM. zout. 2% kruiden en specerijen (SELDERIJ. MOSTERD).  emulgator: E481/SOJALECITHINE. erwteneiwit. kleurstof: E150d.  E= door EU goedgekeurde hulpstoffen." => nil,
-        #"Vulstof (cellulose)| droogextract van sint-janskruid (Hypericum perforatum) 14,7%| verdikkingsmiddel (natriumcarboxymethylcellulose)| glansmiddel (katoenzaadolie)." => nil,
-        #"Zoetstof: Maltitolen. chocolade 15% (cacaomassa. zoetstof: maltitolen. cacaoboter. emulgator: SOJAlecithinen. natuurlijk vanille-aroma). water. TARWEbloem. verstevigingsmiddelen. TARWEzetmeel en maltodextrine. bevochtigingsmiddel: glycerol. EI. zuurteregelaars: citroenzuur en natriumcitraten. geleermiddel; pectines. sinaasappelconcentraat 0.7%. koolzaadolie. rijsmiddel: ammoniumcarbonaten. emulgatoren: mono- en diglyceriden en polyglycerolesters van vetzuren. natuurlijk sinaasappelaroma. zout. kleurstof: carotenen." => nil,
+verbosity = 1
+files = []
+strings = []
+OptionParser.new do |opts|
+  opts.banner = <<-EOF.gsub(/^    /, '')
+    Usage: #{$0} [options] --file|-f <filename>
+           #{$0} [options] --string|-s <ingredients>
 
-        ### Ingredient lists with issues
-        # # pending amount parsing
-        # "Volkorengranen (61%) (HAVER, GERST), rozijnen (10%), suiker, aroma's. Vitaminen en Mineralen: vitaminen (B3/PP, B6) en ijzer. Allergenen: zie ingrediënten, in vet gemarkeerd." => nil,
-        # # pending amount parsing
-        # "Teer 10 mg, nicotine 0,8 mg, koolmonoxide 9 mg" => nil,
-        # # pending amount parsing
-        # "TARWEBLOEM. zout. 1.8% kruiden en specerijen (SELDERIJ. MOSTERD).  emulgator: E481/SOJALECITHINE. erwteneiwit. kleurstof: E150d.  E= door EU goedgekeurde hulpstoffen." => nil,
-        # # missing comma after nested ingredient
-        #"{RIBSTER MINI} (44% mechanisch gescheiden varkensvlees, {tarwe}gluten, ketjap (water, aroma ({soja}), kleurstof: E150d) {soja}-eiwit), {KIPKORN MINI} (28% mechanisch gescheiden kippenvlees, water, {tarwe}gluten, antioxidant: ascorbinezuur, Kan sporen van {ei} bevatten.)" => nil,
-        # # nested ingredients without a name (too many comma's, they belong to previous ingredient)
-        # # also pending amount parsing ("0,1% alcohol")
-        # "Cacaomassa, suiker, 16% mangovulling [suiker, glucosestroop, 1% mangopuree, volle melkpoeder, (LACTOSE, MELKEIWIT), cacaoboter, invertsuikerstroop, limoensap, 0,1% alcohol, natuurlijk aroma, emulgator, (lecithine (SOJA)), verdikkingsmiddel ( johannesbroodpitmeel, guarpitmeel, xanthaangom), kleurstof (caroteen)], 16% chilismaakvulling [suiker, palmvet, raapolie, magere cacaopoeder, cacaomassa, magere melkpoeder (LACTOSE, MELKEIWIT), Lactose (LACTOSE, MELKEIWIT), volle melkpoeder (LACTOSE, MELKEIWIT), wei-eiwit (LACTOSE, MELKEIWIT), dextrose, natuurlijk aroma, emulgator (lecithine (SOJA)), antioxidant, (E306)], cacaoboter, emulgator [lecithine (SOJA)]. " => nil,
-        # # confusing colons, not sure if there is a correct interpretation that is parseable
-        #"Ingredienten: Natuurlijk mineraalwater, sappen uit geconcentreerde sappen uit: 9% sinaasappel, 1% citroen, 1% ananas, 1% mandarijn; voedingszuur: citroenzuur, aroma, zoetstof: sucralose, stevioglycosiden; verrijkt met: vitamine: C, vitamine A." => nil,
-  }
+  EOF
 
-  examples.each do |example, expected|
-    parsed = parser.parse(example)
-    parse_show(example, parsed, inspect: false)
+  opts.on("-f", "--file FILE", "Parse all lines of the file as ingredient lists.") {|f| files << f }
+  opts.on("-s", "--string INGREDIENTS", "Parse specified ingredient list.") {|s| strings << s }
+
+  opts.on("-q", "--[no-]quiet", "Only show summary.") {|q| verbosity = q ? 0 : 1 }
+  opts.on("-v", "--[no-]verbose", "Show more data (parsed tree).") {|v| verbosity = v ? 2 : 1 }
+  opts.on("-h", "--help", "Show this help") do
+    puts(opts)
+    exit
   end
-end
+end.parse!
 
-test_samples(parser, inspect: false)
-#test_tests(parser, inspect: false)
+if strings.any? || files.any?
+  parser = IngredientsParser.new
+  strings.each {|s| parse_single(s, parser: parser, verbosity: verbosity) }
+  files.each   {|f| parse_file(f, parser: parser, verbosity: verbosity) }
+else
+  STDERR.puts("Please specify one or more --file or --string arguments (see --help).")
+end
