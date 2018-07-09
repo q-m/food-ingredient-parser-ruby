4609e1386ed27f5ec55333f53f5c94c1daaa9bbd
wvengen <willem@thequestionmark.org>
2018-07-09 21:39:51 +0200

    Easier use in shellscripts of test output

--- test.rb
+++ test.rb
@@ -7,33 +7,45 @@ require_relative 'ingredients-parser'
 
 begin
   require 'pry'
-  def pp(o)
-    Pry::ColorPrinter.pp(o)
+  def pp(o, color: true)
+    if color
+      Pry::ColorPrinter.pp(o)
+    else
+      puts(o.inspect)
+    end
   end
 rescue loaderror
   # fallback without color printing
-  def pp(o)
+  def pp(o, color: nil)
     puts(o.inspect)
   end
 end
 
-def parse_single(s, parsed=nil, parser: nil, verbosity: 1, print: nil)
+def colorize(color, s)
+  if color
+    "\e[#{color}m#{s}\e[0;22m"
+  else
+    s
+  end
+end
+
+def parse_single(s, parsed=nil, parser: nil, verbosity: 1, print: nil, escape: false, color: false)
   parser ||= IngredientsParser.new
   parsed ||= parser.parse(s)
 
   return unless print.nil? || (parsed && print == :parsed) || (!parsed && print == :noresult)
 
-  puts "\e[0;32m\"#{s}\"\e[0;22m" if verbosity > 0
+  puts colorize(color && "0;32", escape ? s.gsub("\n", "\\n") : s) if verbosity > 0
 
   if parsed
     puts(parsed.inspect) if verbosity > 1
-    pp(parsed.to_h) if verbosity > 0
+    pp(parsed.to_h, color: color) if verbosity > 0
   else
     puts "(no result: #{parser.failure_reason})" if verbosity > 0
   end
 end
 
-def parse_file(path, parser: nil, verbosity: 1, print: nil)
+def parse_file(path, parser: nil, verbosity: 1, print: nil, escape: false, color: false)
   count_parsed = count_noresult = 0
   File.foreach(path) do |line|
     next if line =~ /^#/    # comment
@@ -44,18 +56,20 @@ def parse_file(path, parser: nil, verbosity: 1, print: nil)
     count_parsed += 1 if parsed
     count_noresult += 1 unless parsed
 
-    parse_single(line, parsed, parser: parser, verbosity: verbosity, print: print)
+    parse_single(line, parsed, parser: parser, verbosity: verbosity, print: print, escape: escape, color: color)
   end
 
   pct_parsed   = 100.0 * count_parsed / (count_parsed + count_noresult)
   pct_noresult = 100.0 * count_noresult / (count_parsed + count_noresult)
-  puts "parsed \e[1;32m#{count_parsed}\e[0;22m (#{pct_parsed.round(1)}%), no result \e[1;31m#{count_noresult}\e[0;22m (#{pct_noresult.round(1)}%)"
+  puts "parsed #{colorize(color && "1;32", count_parsed)} (#{pct_parsed.round(1)}%), no result #{colorize(color && "1;31", count_noresult)} (#{pct_noresult.round(1)}%)"
 end
 
 verbosity = 1
 files = []
 strings = []
 print = nil
+escape = false
+color = true
 OptionParser.new do |opts|
   opts.banner = <<-EOF.gsub(/^    /, '')
     Usage: #{$0} [options] --file|-f <filename>
@@ -68,6 +82,8 @@ OptionParser.new do |opts|
 
   opts.on("-q", "--[no-]quiet", "Only show summary.") {|q| verbosity = q ? 0 : 1 }
   opts.on("-p", "--parsed", "Only show lines that were successfully parsed.") {|p| print = :parsed }
+  opts.on("-e", "--escape", "Escape newlines") {|e| escape = true }
+  opts.on("-c", "--[no-]color", "Use color") {|e| color = !!e }
   opts.on("-n", "--noresult", "Only show lines that had no result.") {|p| print = :noresult }
   opts.on("-v", "--[no-]verbose", "Show more data (parsed tree).") {|v| verbosity = v ? 2 : 1 }
   opts.on("-h", "--help", "Show this help") do
@@ -78,8 +94,8 @@ end.parse!
 
 if strings.any? || files.any?
   parser = IngredientsParser.new
-  strings.each {|s| parse_single(s, parser: parser, verbosity: verbosity, print: print) }
-  files.each   {|f| parse_file(f, parser: parser, verbosity: verbosity, print: print) }
+  strings.each {|s| parse_single(s, parser: parser, verbosity: verbosity, print: print, escape: escape, color: color) }
+  files.each   {|f| parse_file(f, parser: parser, verbosity: verbosity, print: print, escape: escape, color: color) }
 else
   STDERR.puts("Please specify one or more --file or --string arguments (see --help).")
 end
