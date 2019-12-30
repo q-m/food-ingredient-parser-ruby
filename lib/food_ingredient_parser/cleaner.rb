ea6f17f2b1db021046fb5aae3d456109203a4650
wvengen <willem@thequestionmark.org>
2019-12-30 15:57:40 +0100

    Normalize unicode comma

--- lib/food_ingredient_parser/cleaner.rb
+++ lib/food_ingredient_parser/cleaner.rb
@@ -4,6 +4,7 @@ module FoodIngredientParser
     def self.clean(s)
       s.gsub!("\u00ad", "")             # strip soft hyphen
       s.gsub!("\u0092", "'")            # windows-1252 apostrophe - https://stackoverflow.com/a/15564279/2866660
+      s.gsub!("‚", ",")                 # normalize unicode comma
       s.gsub!("aÄs", "aïs")             # encoding issue for maïs
       s.gsub!("Ã¯", "ï")                # encoding issue
       s.gsub!("Ã«", "ë")                # encoding issue
