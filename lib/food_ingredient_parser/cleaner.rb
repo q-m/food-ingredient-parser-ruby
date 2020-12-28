0e4f7ee6324ca39b9a4fca8da5d787b1f98ba2ea
wvengen <willem@thequestionmark.org>
2020-12-28 13:20:08 +0100

    Cleanup sporadically occuring newline encoding

--- lib/food_ingredient_parser/cleaner.rb
+++ lib/food_ingredient_parser/cleaner.rb
@@ -2,12 +2,13 @@ module FoodIngredientParser
   module Cleaner
 
     def self.clean(s)
-      s.gsub!("\u00ad", "")             # strip soft hyphen
-      s.gsub!("\u0092", "'")            # windows-1252 apostrophe - https://stackoverflow.com/a/15564279/2866660
-      s.gsub!("‚", ",")                 # normalize unicode comma
-      s.gsub!("aÄs", "aïs")             # encoding issue for maïs
-      s.gsub!("Ã¯", "ï")                # encoding issue
-      s.gsub!("Ã«", "ë")                # encoding issue
+      s.gsub!(/(_x005f_|_)x000d_/i, "\n") # fix sometimes encoding for newline
+      s.gsub!("\u00ad", "")               # strip soft hyphen
+      s.gsub!("\u0092", "'")              # windows-1252 apostrophe - https://stackoverflow.com/a/15564279/2866660
+      s.gsub!("‚", ",")                   # normalize unicode comma
+      s.gsub!("aÄs", "aïs")               # encoding issue for maïs
+      s.gsub!("Ã¯", "ï")                  # encoding issue
+      s.gsub!("Ã«", "ë")                  # encoding issue
       s.gsub!(/\A\s*(["']+)(.*)\1\s*\z/, '\2') # enclosing quotation marks
       s
     end
